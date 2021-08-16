7fe5370070df05feed7f8fa7d81811b5122bb0d4
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2021-08-15 22:35:59 -0400

    Cleanup

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="flex flex-col items-center h-screen md:flex-row">
+  <section class="flex flex-col items-center md:flex-row mt-40">
     <div class="container mx-auto">
       <div class="flex justify-center px-2 py-6">
         <div class="flex w-full rounded-lg xl:w-3/4 lg:w-11/12 lg:shadow-xl">
@@ -19,25 +19,6 @@
           </div>
           <div class="w-full px-8 py-24 bg-white rounded-lg border-gray-100 lg:w-8/12 lg:px-24 lg:py-4 lg:rounded-l-none">
             <div class="relative z-10 text-left ">
-              <div class="flex justify-center mx-auto w-56 lg:py-6">
-                <button type="button" class="inline-flex w-full px-4 py-3 font-semibold text-black transition duration-500 ease-in-out transform bg-white border rounded-lg border-gray-300 hover:bg-black hover:text-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
-                  <div class="flex items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="w-6 h-6" viewBox="0 0 48 48">
-                      <defs>
-                        <path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z" />
-                      </defs>
-                      <clipPath id="b">
-                        <use xlink:href="#a" overflow="visible" />
-                      </clipPath>
-                      <path clip-path="url(#b)" fill="#FBBC05" d="M0 37V11l17 13z" />
-                      <path clip-path="url(#b)" fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z" />
-                      <path clip-path="url(#b)" fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z" />
-                      <path clip-path="url(#b)" fill="#4285F4" d="M48 48L17 24l-4-3 35-10z" />
-                    </svg>
-                    <span class="ml-4"> Log in with Google </span>
-                  </div>
-                </button>
-              </div>
               <form class="mt-6" action="#" method="POST">
                 <div>
                   <label class="block text-base font-medium leading-relaxed text-gray-700">User Name</label>
@@ -104,9 +85,6 @@ export default Vue.extend({
   },
   methods: {
     login () {
-      console.log('hello world')
-      // await this.$axios.get('http://localhost:8000/sanctum/csrf-cookie')
-
       this.$auth.loginWith('laravelSanctum', {
         data: {
           email: 'zaczajdel213@gmail.com',
