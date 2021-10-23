f590475fd8ac43aa9c09c6d3fc77fcaaff22e8bf
Zachary Zajdel <42820232+Zac-Zajdel@users.noreply.github.com>
2021-10-23 01:10:58 -0400

    Added Model, Policy, Factory, Seeder

---
+++ app/Models/Blog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Blog extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $casts = ['body' => AsArrayObject::class];
+
+    /**
+     * Finds associated creator of blog
+     * 
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      User::create([
-        'name' => 'Zac',
-        'email' => 'zaczajdel213@gmail.com',
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-      ]);
+        User::create([
+            'name' => 'Zac',
+            'email' => 'zaczajdel213@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        ]);
+
+        $this->call([
+            BlogSeeder::class,
+        ]);
     }
 }
